2b8491d3b240a60a0eb50e274678639b52ed9c23
Vijay Shukla <shukla.vijay@outlook.com>
2021-05-15 19:40:30 +0100

    temp

---
+++ SampleApiDemo.Helper/Descriptor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace SampleApiDemo.Helper
+{
+    public class Descriptor
+    {
+        private IConfiguration _configuration;
+
+        public Descriptor(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+
+        public string GetEnvironmentDescription(string envName)
+        {
+            switch (envName)
+            {
+                case "Audit-Prod":
+                    return this._configuration.GetSection($"ServiceSettings:ServiceOne:Description").Value;
+                    break;
+
+                case "Security-Prod":
+                    return this._configuration.GetSection($"ServiceSettings:ServiceTwo:Description").Value;
+                    break;
+
+                default:
+                    return "No match";
+            }
+        }
+    }
+}
